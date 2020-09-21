98e8bcd7bcfbfbd8715b2e09fc49a8ce0cf251b3
Simen Holmestad <simehol@stud.ntnu.no>
2020-09-21 15:26:45 +0200

    Add files for handling xgeo-data

---
+++ src/apis/xgeo/xgeo.py
@@ -0,0 +1,6 @@
+import apis.api as api
+
+
+class Xgeo(api.Processor):
+    def get_data(self):
+        pass
---
+++ src/apis/xgeo/xgeo_fetcher.py
@@ -0,0 +1,6 @@
+import apis.fetcher as fetcher
+
+
+class XgeoFetcher(fetcher.Fetcher):
+    def fetch(self):
+        pass
