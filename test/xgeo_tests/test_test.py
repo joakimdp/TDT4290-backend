23181c1b36aa53f7d1d63766b78d02a807ef0b02
Simen Holmestad <simehol@stud.ntnu.no>
2020-09-25 11:46:08 +0200

    Add fetching of data from xgeo

--- test/xgeo_tests/test_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import apis.xgeo.xgeo_fetcher as x
-
-
-class TestStringMethods(unittest.TestCase):
-    def test_xgeo_fetcher(self):
-        result = x.XgeoFetcher().fetch(1)
-        self.assertEqual(result[0][0], "2020-09-15")
