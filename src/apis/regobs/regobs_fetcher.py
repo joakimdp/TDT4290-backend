b5a780b1c79d0c16cadb44d36e5c8d04676f789d
Mattis Levik Trygstad <mtr@genus.no>
2020-09-25 16:02:58 +0200

    wip: regobs_fetcher

--- src/apis/regobs/regobs_fetcher.py
+++ src/apis/regobs/regobs_fetcher.py
@@ -1,41 +1,57 @@
-#import apis.fetcher as fetcher
+import apis.fetcher as fetcher
 import requests
-from pandas.io.json import json_normalize
 import pandas as pd
-import pyodbc
 
 
-class RegobsFetcher():
-    def fetch(self):
-        result = requests.get(
-            "https://api.nve.no/hydrology/regobs/v3.2.0/Odata.svc/AvalancheObs/?$filter=AvalancheTriggerTID eq 21 or AvalancheTriggerTID eq 26 or AvalancheTriggerTID eq 27&$format=json")
-        print(result.status_code)
+class RegobsFetcher(fetcher.Fetcher):
+    def fetch(self) -> pd.DataFrame:
+        # TODO: move urls
+        avalanche_obs_url = "https://api.nve.no/hydrology/regobs/v3.2.0/Odata.svc/AvalancheObs/?$filter=AvalancheTriggerTID eq 21 or AvalancheTriggerTID eq 26 or AvalancheTriggerTID eq 27&$format=json"
+        incident_url = "https://api.nve.no/hydrology/regobs/v3.2.0/Odata.svc/Incident/?$filter=GeoHazardTID eq 10 and ( DamageExtentTID eq 27 or DamageExtentTID eq 28 or DamageExtentTID eq 29 or DamageExtentTID eq 30 or DamageExtentTID eq 40 )&$format=json"
 
-        json = result.json()['d']['results']
+        avalanche_obs = self.__fetch_from_api(avalanche_obs_url)
+        incident = self.__fetch_from_api(incident_url)
 
-        # for element in json:
-        #    print(element['RegID'])
+        self.regobs_df = pd.merge(avalanche_obs, incident, on=[
+                                  'RegID'], how='outer')
+        # Combine identical columns
+        self.__combine_columns(
+            'Registration.__deferred.uri', 'RegistrationUrl')
+        self.__combine_columns('UsageFlagTID')
+        self.__combine_columns('Comment')
+        self.__combine_columns('__metadata.id')
+        self.__combine_columns('__metadata.uri')
+        self.__combine_columns('__metadata.type')
+        return self.regobs_df
 
-        df = pd.DataFrame.from_dict(json_normalize(json), orient='columns')
+    def __combine_columns(self, column_name: str, new_name: str = None):
+        if new_name == None:
+            new_name = column_name
 
-        print(df)
+        print(new_name)
+        self.regobs_df[new_name] = self.regobs_df[column_name + '_x'].combine_first(
+            self.regobs_df[column_name + '_y'])
+        self.regobs_df = self.regobs_df.drop(
+            columns=[column_name + '_x', column_name + '_y'])
 
-        server = 'tcp:avalanche-server.database.windows.net,1433'
-        database = 'avalanche-db'
-        username = 'admin_user'
-        password = '3S#@7aa2tH'
-        cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
-                              server+';DATABASE='+database+';UID='+username+';PWD=' + password)
-        cursor = cnxn.cursor()
+    def __fetch_from_api(self, url: str) -> pd.DataFrame:
+        data = requests.get(url)
+        json = data.json()['d']['results']
+        data_frame = pd.DataFrame.from_dict(
+            pd.json_normalize(json), orient='columns')
 
-        cursor.execute("SELECT @@version;")
-        row = cursor.fetchone()
-        while row:
-            print(row[0])
-            row = cursor.fetchone()
+        if len(data_frame) != 1000:
+            return data_frame
 
+        # If the dataset contains more than 1000 records, iterate over each new set and append to data_frame
+        last_id = json[-1]['RegID']
+        next_data_frame = data_frame
 
-if __name__ == "__main__":
-    regObsFetcher = RegobsFetcher()
-
-    regObsFetcher.fetch()
+        while len(next_data_frame) == 1000:
+            next_data = requests.get(url + '&$skiptoken=' + str(last_id))
+            next_json = next_data.json()['d']['results']
+            last_id = next_json[-1]['RegID']
+            next_data_frame = pd.DataFrame.from_dict(
+                pd.json_normalize(next_json), orient='columns')
+            data_frame = data_frame.append(next_data_frame)
+        return data_frame
