4b70035088e4d89fafbd0005af9f7d3acd34eef7
Simen Holmestad <simehol@stud.ntnu.no>
2020-09-28 11:34:44 +0200

    Change xgeo_fetcher to return dicts of pd dataframes
    Returning just the raw responses was a mistake

--- test/xgeo_tests/test_xgeo_fetcher.py
+++ test/xgeo_tests/test_xgeo_fetcher.py
@@ -47,6 +47,61 @@ class TestXgeoFetcher(unittest.TestCase):
         self.assertEquals(response[0]["LegendText"], "179633;6782269 (519 moh.), Døgnnedbør v2.0")
         self.assertTrue(len(response[0]["SeriesPoints"]) != 0)
 
+    def test_convert_json_response_to_value_list(self):
+        example_json_response = [{
+            'LegendText': '179633;6782269 (519 moh.), Døgntemperatur v2.0',
+            'SeriesPoints': [
+                {'Key': '/Date(1549753200000)/', 'Value': -2.6500000953674316, 'CorrectionMark': 0},
+                {'Key': '/Date(1549839600000)/', 'Value': -5.050000190734863, 'CorrectionMark': 0},
+                {'Key': '/Date(1549926000000)/', 'Value': -5.25, 'CorrectionMark': 0},
+                {'Key': '/Date(1550012400000)/', 'Value': -3.6500000953674316, 'CorrectionMark': 0},
+                {'Key': '/Date(1550098800000)/', 'Value': 1.0499999523162842, 'CorrectionMark': 0},
+                {'Key': '/Date(1550185200000)/', 'Value': 5.150000095367432, 'CorrectionMark': 0},
+                {'Key': '/Date(1550271600000)/', 'Value': -0.6499999761581421, 'CorrectionMark': 0},
+                {'Key': '/Date(1550358000000)/', 'Value': 0.44999998807907104, 'CorrectionMark': 0},
+                {'Key': '/Date(1550444400000)/', 'Value': 0.05000000074505806, 'CorrectionMark': 0},
+                {'Key': '/Date(1550530800000)/', 'Value': 2.549999952316284, 'CorrectionMark': 0},
+                {'Key': '/Date(1550617200000)/', 'Value': 0.8500000238418579, 'CorrectionMark': 0}],
+            'Statistics': []
+        }]
+        converted_response = XgeoFetcher.convert_json_response_to_value_list(example_json_response)
+        expected_converted_response = [
+            -2.6500000953674316,
+            -5.050000190734863,
+            -5.25,
+            -3.6500000953674316,
+            1.0499999523162842,
+            5.150000095367432,
+            -0.6499999761581421,
+            0.44999998807907104,
+            0.05000000074505806,
+            2.549999952316284,
+            0.8500000238418579
+        ]
+        self.assertEqual(converted_response, expected_converted_response)
+
+    def test_generate_indices(self):
+        example_json_response = [{
+            'LegendText': '179633;6782269 (519 moh.), Nysnødybde 3 døgn',
+            'SeriesPoints': [
+                {'Key': '/Date(1549753200000)/', 'Value': 24.700000762939453, 'CorrectionMark': 0},
+                {'Key': '/Date(1549839600000)/', 'Value': 11.899999618530273, 'CorrectionMark': 0},
+                {'Key': '/Date(1549926000000)/', 'Value': 3, 'CorrectionMark': 0},
+                {'Key': '/Date(1550012400000)/', 'Value': 0.6000000238418579, 'CorrectionMark': 0},
+                {'Key': '/Date(1550098800000)/', 'Value': 0, 'CorrectionMark': 0},
+                {'Key': '/Date(1550185200000)/', 'Value': 0, 'CorrectionMark': 0},
+                {'Key': '/Date(1550271600000)/', 'Value': 0, 'CorrectionMark': 0},
+                {'Key': '/Date(1550358000000)/', 'Value': 0, 'CorrectionMark': 0},
+                {'Key': '/Date(1550444400000)/', 'Value': 0, 'CorrectionMark': 0},
+                {'Key': '/Date(1550530800000)/', 'Value': 0, 'CorrectionMark': 0},
+                {'Key': '/Date(1550617200000)/', 'Value': 0, 'CorrectionMark': 0}],
+            'Statistics': []
+        }]
+
+        indices = XgeoFetcher.generate_date_indices(example_json_response)
+        expected_indices = ['2019-02-10', '2019-02-11', '2019-02-12', '2019-02-13', '2019-02-14', '2019-02-15', '2019-02-16', '2019-02-17', '2019-02-18', '2019-02-19', '2019-02-20']
+        self.assertEquals(indices, expected_indices)
+
     def test_fetch_data_for_avalanche_incident(self):
         avalanche_incident = AvalancheIncident(
             id=1,
@@ -54,10 +109,17 @@ class TestXgeoFetcher(unittest.TestCase):
             coords_utm=(179633, 6782269),
             coords_latlng=(61.044338, 9.062769)
         )
-        incident_xgeo_data = XgeoFetcher.fetch_data_for_avalanche_incident(avalanche_incident)
-        self.assertEquals(incident_xgeo_data["id"], 1)
-        self.assertEquals(incident_xgeo_data["rainfall"][0]["LegendText"], "179633;6782269 (519 moh.), Døgnnedbør v2.0")
-        self.assertTrue(len(incident_xgeo_data["snow_depth"][0]["SeriesPoints"]) != 0)
+        dataframe = XgeoFetcher.fetch_data_for_avalanche_incident(avalanche_incident)
+
+        # Get the column names of the dataframe from XgeoFetcher
+        column_names = list(map(lambda data_code: data_code[1],
+                                XgeoFetcher.DATA_CODE_LIST))
+
+        first_column_name = column_names[0]
+
+        self.assertEqual(dataframe.index[0], "2019-02-10")
+        self.assertIn(first_column_name, dataframe.columns)
+        self.assertTrue(len(dataframe[first_column_name]) != 0)
 
     def test_fetch(self):
         avalanche_incident_list = []
@@ -75,10 +137,21 @@ class TestXgeoFetcher(unittest.TestCase):
             coords_latlng=(60.474065, 7.882042)
         ))
 
-        raw_data = XgeoFetcher().fetch(avalanche_incident_list)
+        dataframe_dict = XgeoFetcher().fetch(avalanche_incident_list)
 
-        self.assertEqual(raw_data[0]["id"], 1)
-        self.assertEqual(raw_data[1]["id"], 2)
+        self.assertEqual(dataframe_dict[1].index[0], "2019-02-10")
+        self.assertEqual(dataframe_dict[1].index[1], "2019-02-11")
 
-        self.assertEqual(raw_data[0]["rainfall"][0]["LegendText"], "179633;6782269 (519 moh.), Døgnnedbør v2.0")
-        self.assertEqual(raw_data[1]["rainfall"][0]["LegendText"], "109190;6725372 (1229 moh.), Døgnnedbør v2.0")
+        self.assertEqual(dataframe_dict[2].index[0], "2019-02-10")
+        self.assertEqual(dataframe_dict[2].index[1], "2019-02-11")
+
+        # Get the column names of the dataframe from XgeoFetcher
+        column_names = list(map(lambda data_code: data_code[1],
+                                XgeoFetcher.DATA_CODE_LIST))
+
+        first_column_name = column_names[0]
+        second_column_name = column_names[1]
+        self.assertIn(first_column_name, dataframe_dict[1].columns)
+        self.assertIn(first_column_name, dataframe_dict[2].columns)
+        self.assertIn(second_column_name, dataframe_dict[1].columns)
+        self.assertIn(second_column_name, dataframe_dict[2].columns)
