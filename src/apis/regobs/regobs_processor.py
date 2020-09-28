235516ae661a72c4dfbcd898eaa11a1bee94c00a
Johannes Åsheim <johannes.aasheim@hotmail.com>
2020-09-28 12:09:52 +0000

    Saf 122 preprocess regobs

--- src/apis/regobs/regobs_processor.py
+++ src/apis/regobs/regobs_processor.py
@@ -1,6 +1,32 @@
 import apis.processor as processor
-
+import pandas as pd
+import math
+import util.utm_converter as utm_converter
 
 class RegobsProcessor(processor.Processor):
+
+    def __init__(self, df: pd.DataFrame):
+        self.df = df
+    
     def process(self):
-        pass
+        '''
+        Add columns with latlong coordinates
+        '''
+        lat = []
+        lon = []
+
+        for index, row in self.df.iterrows():
+            utmEast = int(row["UTMEast"])
+            utmNorth = int(row["UTMNorth"])
+
+            coor = utm_converter.convert(utmEast, utmNorth)
+
+            lon.append(float(coor[1]))
+            lat.append(float(coor[0]))
+
+        self.df["Lon"] = lon
+        self.df["Lat"] = lat
+
+        return self.df
+
+ 
--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
-# TODO: Implement this
-
 from apis.regobs.regobs_fetcher import RegobsFetcher
-
+from apis.regobs.regobs_processor import RegobsProcessor
+import utm
 
 if __name__ == "__main__":
+    
     regobs_fetcher = RegobsFetcher()
     data_frame = regobs_fetcher.fetch()
+    
     print(data_frame.isnull().sum())
     print(data_frame)
+    
+    regobs_processor = RegobsProcessor(data_frame)
+    g = regobs_processor.process()
+    print(g)
+    
