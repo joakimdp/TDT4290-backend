7f85d3fd2d9580c0ba4dbfacfa7763ab189f266e
Mattis Levik Trygstad <mtr@genus.no>
2020-09-28 15:35:27 +0200

    wip: regobs_initializer implemented

--- src/apis/regobs/regobs_initializer.py
+++ src/apis/regobs/regobs_initializer.py
@@ -1,6 +1,64 @@
 import apis.initializer as initializer
+from sqlalchemy import create_engine
+import pyodbc
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.types import Date, Float, Integer, String
+from sqlalchemy.engine.base import Engine
+
+Base = declarative_base()
+
+
+class RegobsData(Base):
+    __tablename__ = 'regobs_data'
+    reg_id = Column(Integer, primary_key=True)
+    aspect = Column(Integer)
+    height_start_zone = Column(Integer)
+    height_stop_zone = Column(Integer)
+    destructive_size_tid = Column(Integer)
+    avalanche_trigger_tid = Column(Integer)
+    avalanche_tid = Column(Integer)
+    terrain_start_zone_tid = Column(Integer)
+    utm_zone_stop = Column(Integer)
+    utm_east_stop = Column(Integer)
+    utm_north_stop = Column(Integer)
+    DtAvalancheTime = Column(Date)
+    snow_line = Column(Integer)
+    utm_zone_start = Column(Integer)
+    utm_east_start = Column(Integer)
+    valid_exposition = Column(Integer)
+    aval_cause_tid = Column(Integer)
+    fracture_heigth = Column(Integer)
+    fracture_width = Column(Integer)
+    trajectory = Column(Integer)
+    geo_hazard_tid = Column(Integer)
+    activity_influenced_tid = Column(Integer)
+    damage_extent_tid = Column(Integer)
+    forecast_accurate_tid = Column(Integer)
+    dt_end_time = Column(Date)
+    incident_header = Column(String)
+    incident_ingress = Column(String)
+    incident_text = Column(String)
+    sensitive_text = Column(String)
+    incident_url = Column(String)
+    registration_url = Column(String)
+    usage_flag_tid = Column(Integer)
+    comment = Column(String)
+    metadata_id = Column(Integer)
+    metadata_uri = Column(String)
+    metadata_type = Column(String)
+    utm_east_reg = Column(Integer)
+    utm_north_reg = Column(Integer)
+    lat = Column(Float)
+    lng = Column(Float)
 
 
 class RegobsInitializer(initializer.Initializer):
+
+    def __init__(self, engine: Engine):
+        super().__init__()
+        self.engine = engine
+        print(self.engine.table_names())
+
     def initialize_tables(self):
-        pass
+        RegobsData.metadata.create_all(self.engine)
--- src/main.py
+++ src/main.py
@@ -1,10 +1,36 @@
-# TODO: Implement this
-
 from apis.regobs.regobs_fetcher import RegobsFetcher
+from apis.regobs.regobs_initializer import RegobsInitializer
+from sqlalchemy.engine import create_engine
+from sqlalchemy.engine.base import Engine
+from decouple import config
+
+
+def main():
+    regobs_fetcher = RegobsFetcher()
+    # Uncomment to fetch Regobs data
+    """ data_frame = regobs_fetcher.fetch()
+    print(data_frame.isnull().sum())
+    print(data_frame) """
+
+    engine = create_db_connection()
+
+    regobs_initializer = RegobsInitializer(engine)
+    regobs_initializer.initialize_tables()
+
+
+def create_db_connection() -> Engine:
+    server = 'avalanche-server.database.windows.net,1433'
+    database = 'avalanche-db'
+    username = config('USERNAME')
+    password = config('PASSWORD')
+    driver = 'ODBC Driver 17 for SQL Server'
+
+    connection_string = 'mssql+pyodbc://{username}:{password}@{server}/{database}?driver={driver}?Trusted_Connection=yes'.format(
+        username=username, password=password, server=server, database=database, driver=driver)
+    engine = create_engine(connection_string)
+
+    return engine
 
 
 if __name__ == "__main__":
-    regobs_fetcher = RegobsFetcher()
-    data_frame = regobs_fetcher.fetch()
-    print(data_frame.isnull().sum())
-    print(data_frame)
+    main()
