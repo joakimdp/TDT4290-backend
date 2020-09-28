e9dc0dcd939502a4e84e1765f6040f695cf7a40b
johalie <johanlie@stud.ntnu.no>
2020-09-28 11:42:32 +0200

    Wrote more tests

--- src/apis/skredvarsel/skredvarsel_fetcher.py
+++ src/apis/skredvarsel/skredvarsel_fetcher.py
@@ -1,31 +1,25 @@
-from apis.fetcher as fetcher
+from apis.fetcher import Fetcher
 import requests
 
-class SkredvarselFetcher(fetcher.Fetcher):
+class SkredvarselFetcher(Fetcher):
     def create_url(lat, lon, date):
-        api_format_sting = "https://api01.nve.no/hydrology/forecast/avalanche/v5.0.1/api/AvalancheWarningByCoordinates/Simple/{}/{}/1/{}/"
-     return api_format_sting.format(lat, lon, date)
+        api_format_sting = "https://api01.nve.no/hydrology/forecast/avalanche/v5.0.1/api/AvalancheWarningByCoordinates/Detail/{}/{}/1/{}/{}/"
+        return api_format_sting.format(lat, lon, date, date)
 
     def fetch_data_from_skredvarsel(avalanche_incident):
         api_url = SkredvarselFetcher.create_url(
         date = avalanche_incident.time,
-        lat = avalanche_incident.coords_latlong[0],
-        lon = avalanche_incident.coords_lat_long[1])
-     return request.get(create_url(api_url).json()
+        lat = avalanche_incident.coords_latlng[0],
+        lon = avalanche_incident.coords_latlng[1])
+        return requests.get(api_url).json()
 
-     def fetch_data_for_avalanche_incidents(avalanche_incident):
-         incident_skredvarsel_data = {}
-         incident_skredvarsel_data["id"] = avalanche_incident.id
-         response = SkredvarselFetcher.fetch_data_from_skredvarsel(avalanche_incident)
-         incident_skredvarsel_data["json-data"] = response
-
-        return incident_skredvarsel_data
 
     def fetch(self, avalanche_incident_list):
-        raw_data = []
+        raw_data = {}
         for avalanche_incident in avalanche_incident_list:
-          avalanche_incident_skredvarsel_data = SkredvarselFetcher.fetch_data_for_avalanche_incidents(avalanche_incident)
-          raw_data.append(avalanche_incident_skredvarsel_data)
+            response = SkredvarselFetcher.fetch_data_from_skredvarsel(avalanche_incident)
+            raw_data[avalanche_incident.id] = response[0]
+            
         return raw_data
             
         
