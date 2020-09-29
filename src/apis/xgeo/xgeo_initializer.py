9de29fcab93493123c1f351c19005aa7bfee94f5
Simen Holmestad <simehol@stud.ntnu.no>
2020-09-29 09:20:07 +0200

    Add code for creating regobs database table

--- src/apis/xgeo/xgeo_initializer.py
+++ src/apis/xgeo/xgeo_initializer.py
@@ -1,6 +1,26 @@
 import apis.initializer as initializer
+from sqlalchemy import Column, Integer, Date, Float
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.engine.base import Engine
+
+Base = declarative_base()
+
+
+class XgeoData(Base):
+    __tablename__ = 'xgeo_data'
+    id = Column(Integer, primary_key=True)
+    date = Column(Date, primary_key=True)
+    snow_depth_3_days = Column(Float)
+    snow_depth = Column(Float)
+    rainfall = Column(Float)
+    temperature = Column(Float)
+    wind_speed = Column(Float)
+    wind_direction = Column(Integer)
 
 
 class XgeoInitializer(initializer.Initializer):
+    def __init__(self, engine: Engine):
+        self.engine = engine
+
     def initialize_tables(self):
-        pass
+        XgeoData.metadata.create_all(self.engine)
