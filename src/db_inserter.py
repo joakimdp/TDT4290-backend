a92b862e02fdfadce6636c3e1a46fe9ab6d67478
Simen Holmestad <simehol@stud.ntnu.no>
2020-10-05 10:12:20 +0200

    Change main method to be more general
    Adding a new API should be much easier now

--- src/db_inserter.py
+++ src/db_inserter.py
@@ -13,4 +13,4 @@ class DbInserter():
 
     def insert(self, table_name: str, data_frame: pd.DataFrame, if_exists: str) -> None:
         data_frame.to_sql(table_name, con=self.engine,
-                          if_exists=if_exists, chunksize=50, method='multi')
+                          if_exists=if_exists, chunksize=5, method='multi')
