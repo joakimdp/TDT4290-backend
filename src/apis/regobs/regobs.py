7190dc9601a3c5211c1d82840e0c8c87ac23528d
Joakim <joakimdp@stud.ntnu.no>
2020-11-18 09:59:29 +0100

    Set up for project demo

--- src/apis/regobs/regobs.py
+++ src/apis/regobs/regobs.py
@@ -1,11 +1,15 @@
 from apis.api import Api
 from apis.regobs.regobs_fetcher import RegobsFetcher
 from apis.regobs.regobs_processor import RegobsProcessor
+from util.csv import to_csv, read_csv
 
 
 class Regobs(Api):
     def get_data(self):
-        regobs_fetcher = RegobsFetcher()
-        fetched_regobs_data = regobs_fetcher.fetch()
-        regobs_processor = RegobsProcessor()
-        return regobs_processor.process(fetched_regobs_data)
+        # regobs_fetcher = RegobsFetcher()
+        # fetched_regobs_data = regobs_fetcher.fetch()
+        # regobs_processor = RegobsProcessor()
+        # processed = regobs_processor.process(fetched_regobs_data)
+        # to_csv(processed, 'csv_files/regobs-backup.csv')
+        processed = read_csv('csv_files/regobs-6.csv')
+        return processed
